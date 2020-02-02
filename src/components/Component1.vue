207d440d568dae7909f4c923e54c88da54f53ef2
Congwei Bai <congweibai@gmail.com>
2020-02-02 21:41:36 +1030

    challenge2

--- src/components/Component1.vue
+++ src/components/Component1.vue
@@ -1,7 +1,7 @@
 <template>
-    <div v-if="string.length > 0">
-        <p>Congrats you have discovered the issue</p>
-        <p>Don't forget to commit this solution</p>
+    <div v-if="(text1.length + text2.length) > 0">
+        <p>{{text1}}</p>
+        <p>{{text2}}</p>
     </div>
 </template>
 
@@ -10,7 +10,10 @@
         name: 'Component1',
         data() {
             return {
-                string: null
+                text1:"Congrats you have discovered the issue",
+                text2:"Don't forget to commit this solution"
+                //I would not use string as a varibale name as it is variable type
+                // string: null
             }
         }
     }
